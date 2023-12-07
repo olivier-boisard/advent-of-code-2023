13702706ea054e43b979216312263b293bc83145
Olivier Boisard <olivier.boisard@merckgroup.com>
2023-12-07 16:39:24 +0100

    Refactored part2.

--- src/day05/part2.py
+++ src/day05/part2.py
@@ -1,23 +1,16 @@
 from typing import Iterable
 
-import tqdm
-
-from day05.extraction import extract_maps_and_seeds
+from day05.extraction import extract_map_and_seeds
 
 
 def solve(puzzle_input: Iterable[str]) -> int:
-    all_maps, seed_ranges = extract_maps_and_seeds(puzzle_input)
+    mapper, seed_ranges = extract_map_and_seeds(puzzle_input)
 
     seed_range_starts = seed_ranges[0::2]
     seed_range_lengths = seed_ranges[1::2]
 
-    min_number = None
+    min_numbers = []
     for seed_range_start, seed_range_length in zip(seed_range_starts, seed_range_lengths):
-        for number in range(seed_range_start, seed_range_start + seed_range_length):
-            for range_map in all_maps:
-                number = range_map[number]
-            if min_number is None:
-                min_number = number
-            else:
-                min_number = min(min_number, number)
-    return min_number
+        seed_range_stop = seed_range_start + seed_range_length
+        min_numbers.append(min(*[mapper(number) for number in range(seed_range_start, seed_range_stop)]))
+    return min(min_numbers)
