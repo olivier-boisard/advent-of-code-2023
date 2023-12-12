6474ca692744d93ff347a8f4d5ecebb088d0383a
Olivier Boisard <olivier.boisard@merckgroup.com>
2023-12-12 14:21:04 +0100

    Refactored part1.

--- src/day06/part1.py
+++ src/day06/part1.py
@@ -1,6 +1,8 @@
 import re
 from dataclasses import dataclass
-from typing import Iterable
+from functools import reduce
+from math import prod
+from typing import Iterable, Iterator
 
 
 @dataclass
@@ -10,28 +12,33 @@ class _RaceRecord:
 
 
 def solve(puzzle_input: Iterable[str]) -> int:
+    race_records = _extract_race_records(puzzle_input)
+    return prod(_compute_n_options_to_beat_record(race_record) for race_record in race_records)
+
+
+def _extract_race_records(puzzle_input: Iterable[str]) -> Iterator[_RaceRecord]:
     puzzle_input = list(puzzle_input)
     times_str = re.findall(r'\d+ ?', puzzle_input[0])
     distances_str = re.findall(r'\d+ ?', puzzle_input[1])
-    race_records = []
     for time_str, distance_str in zip(times_str, distances_str):
-        race_records.append(_RaceRecord(time=int(time_str), distance=int(distance_str)))
+        yield _RaceRecord(time=int(time_str), distance=int(distance_str))
 
-    n_options = 1
-    for race_record in race_records:
-        half_time = race_record.time // 2
-        upper_bound = half_time
-        lower_bound = 0
-        while lower_bound + 1 != upper_bound:
-            current_speed = (lower_bound + upper_bound) // 2
-            remaining_race_time = race_record.time - current_speed
-            distance = remaining_race_time * current_speed
-            if distance > race_record.distance:
-                upper_bound = current_speed
-            else:
-                lower_bound = current_speed
-        factor = (half_time - upper_bound + 1) * 2
-        if race_record.time % 2 == 0:
-            factor -= 1
-        n_options *= factor
+
+def _compute_n_options_to_beat_record(race_record):
+    half_time = race_record.time // 2
+    upper_bound = half_time
+    lower_bound = 0
+    while lower_bound + 1 != upper_bound:
+        current_speed = (lower_bound + upper_bound) // 2
+        remaining_race_time = race_record.time - current_speed
+        distance = remaining_race_time * current_speed
+        if distance > race_record.distance:
+            upper_bound = current_speed
+        else:
+            lower_bound = current_speed
+
+    n_options = (half_time - upper_bound + 1) * 2
+    if race_record.time % 2 == 0:
+        n_options -= 1
+
     return n_options
