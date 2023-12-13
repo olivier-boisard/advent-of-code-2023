80b81c90256be3691cbf6d26aec6ef441422335c
Olivier Boisard <olivier.boisard@merckgroup.com>
2023-12-13 12:26:46 +0100

    Simplified code.

--- src/day07/part2.py
+++ src/day07/part2.py
@@ -11,7 +11,7 @@ def solve(puzzle_input: Iterable[str]) -> int:
 def _joker_strength_computer(hand: str) -> HandType:
     jocker_str = 'J'
     n_most_common_cards = 2
-    hand_without_jokers = hand.replace(jocker_str, '')
-    most_common_cards = Counter(hand_without_jokers).most_common(n_most_common_cards)
+
+    most_common_cards = Counter(hand.replace(jocker_str, '')).most_common(n_most_common_cards)
     replacement_card = most_common_cards[0][0] if len(most_common_cards) > 0 else 'A'
     return standard_strength_computer(hand.replace(jocker_str, replacement_card))
