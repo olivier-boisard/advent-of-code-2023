c171464eb070b0f3cf650204f62b666a349e2de6
Olivier Boisard <olivier.boisard@merckgroup.com>
2023-12-13 11:43:21 +0100

    Refactored part 2 draft.

--- src/day07/hand.py
+++ src/day07/hand.py
@@ -7,7 +7,7 @@ FIGURE_TO_INT = {'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
 INT_TO_FIGURE = {value: key for key, value in FIGURE_TO_INT.items()}
 
 
-class Strength(IntEnum):
+class HandType(IntEnum):
     FIVE_OF_A_KIND = 7
     FOUR_OF_A_KIND = 6
     FULL_HOUSE = 5
@@ -21,33 +21,33 @@ class Strength(IntEnum):
 class HandWithBid:
     hand: str
     bid: int
-    strength_func: Callable[[str], Strength]
+    strength_func: Callable[[str], HandType]
 
     def __int__(self):
-        max_digit_value = max(FIGURE_TO_INT.values())
         output = 0
+        base = max(FIGURE_TO_INT.values()) + 1
         for i, card in enumerate(self.hand[::-1]):
-            output += max_digit_value ** i * (FIGURE_TO_INT[card] if card in FIGURE_TO_INT else int(card))
-        return output + self.strength * max_digit_value ** (len(self.hand) + 1)
+            output += base ** i * (FIGURE_TO_INT[card] if card in FIGURE_TO_INT else int(card))
+        return output + self.hand_type * base ** len(self.hand)
 
     @property
-    def strength(self) -> Strength:
+    def hand_type(self) -> HandType:
         return self.strength_func(self.hand)
 
 
-def standard_strength_computer(hand: str) -> Strength:
+def standard_strength_computer(hand: str) -> HandType:
     highest_occurrences_count = max(Counter(hand).values())
     n_different_cards = len(set(hand))
     if n_different_cards == 1:
-        output = Strength.FIVE_OF_A_KIND
+        output = HandType.FIVE_OF_A_KIND
     elif n_different_cards == 2:
-        output = Strength.FOUR_OF_A_KIND if highest_occurrences_count == 4 else Strength.FULL_HOUSE
+        output = HandType.FOUR_OF_A_KIND if highest_occurrences_count == 4 else HandType.FULL_HOUSE
     elif n_different_cards == 3:
-        output = Strength.THREE_OF_A_KIND if highest_occurrences_count == 3 else Strength.TWO_PAIRS
+        output = HandType.THREE_OF_A_KIND if highest_occurrences_count == 3 else HandType.TWO_PAIRS
     elif n_different_cards == 4:
-        output = Strength.ONE_PAIR
+        output = HandType.ONE_PAIR
     else:
-        output = Strength.HIGH_CARD
+        output = HandType.HIGH_CARD
     return output
 
 
--- src/day07/part2.py
+++ src/day07/part2.py
@@ -1,14 +1,14 @@
 from collections import Counter
-from typing import Iterable
+from typing import Iterable, Tuple, List
 
-from day07.hand import Strength, standard_strength_computer, run_game, FIGURE_TO_INT, INT_TO_FIGURE
+from day07.hand import HandType, standard_strength_computer, run_game, FIGURE_TO_INT, INT_TO_FIGURE
 
 
 def solve(puzzle_input: Iterable[str]) -> int:
-    return run_game(puzzle_input, _jocker_strength_computer)
+    return run_game(puzzle_input, _joker_strength_computer)
 
 
-def _jocker_strength_computer(hand: str) -> Strength:
+def _joker_strength_computer(hand: str) -> HandType:
     jocker_str = 'J'
     n_most_common_cards = 2
     hand_without_jokers = hand.replace(jocker_str, '')
@@ -20,21 +20,31 @@ def _jocker_strength_computer(hand: str) -> Strength:
         most_common_card = most_common_card_count[0]
         replacement_card = most_common_card
         if most_common_card_count[1] == 1:  # if each card is different
-            card_values = []
-            for card in hand_without_jokers:
-                card_values.append(FIGURE_TO_INT[card] if card in FIGURE_TO_INT else int(card))
-            max_card_value = max(card_values)
-            replacement_card = INT_TO_FIGURE[max_card_value] if max_card_value in INT_TO_FIGURE else str(max_card_value)
+            replacement_card = _extract_most_powerful_card(hand_without_jokers)
         else:
             if len(most_common_cards) == n_most_common_cards:
                 second_most_common_card_count = most_common_cards[1]
                 if most_common_card_count[1] == 2 and second_most_common_card_count[1] == 2:  # if there are two pairs
-                    second_most_common_card = second_most_common_card_count[0]
-                    highest_pair_value = max(
-                        FIGURE_TO_INT[most_common_card] if most_common_card in FIGURE_TO_INT else int(most_common_card),
-                        FIGURE_TO_INT[second_most_common_card] if second_most_common_card in FIGURE_TO_INT else int(second_most_common_card)
-                    )
-                    replacement_card = INT_TO_FIGURE[highest_pair_value] if highest_pair_value in INT_TO_FIGURE else str(highest_pair_value)
+                    replacement_card = _extract_highest_pair_card(most_common_cards)
     hand = hand.replace(jocker_str, replacement_card)
 
     return standard_strength_computer(hand)
+
+
+def _extract_highest_pair_card(most_common_cards: List[Tuple[str, int]]) -> str:
+    most_common_card = most_common_cards[0][0]
+    second_most_common_card = most_common_cards[1][0]
+    highest_pair_value = max(_compute_card_value(most_common_card), _compute_card_value(second_most_common_card))
+    return INT_TO_FIGURE[highest_pair_value] if highest_pair_value in INT_TO_FIGURE else str(highest_pair_value)
+
+
+def _compute_card_value(card: str) -> int:
+    return FIGURE_TO_INT[card] if card in FIGURE_TO_INT else int(card)
+
+
+def _extract_most_powerful_card(hand_without_jokers):
+    card_values = []
+    for card in hand_without_jokers:
+        card_values.append(FIGURE_TO_INT[card] if card in FIGURE_TO_INT else int(card))
+    max_card_value = max(card_values)
+    return INT_TO_FIGURE[max_card_value] if max_card_value in INT_TO_FIGURE else str(max_card_value)
